1243c762ec055f198e7ef27fe36e3ae2928f7af7
mh-hashemian <hashemian0617@gmail.com>
2023-07-10 16:12:36 +0330

    initilize project with bootstrap and ...

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from 'reactstrap'
+
+function App() {
+
+  return (
+    <>
+     <Button className='fw-light' color="primary">Click</Button>
+    </>
+  )
+}
+
+export default App
